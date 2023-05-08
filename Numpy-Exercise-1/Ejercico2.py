4da5496dea1618c3fe2b5be36f3ece8132d4dfcb
Farid Rojas <111253744+FaridRojas@users.noreply.github.com>
2023-05-08 10:17:02 -0500

    Actualizacion ejercicio 2

--- Numpy-Exercise-1/Ejercico2.py
+++ Numpy-Exercise-1/Ejercico2.py
@@ -3,9 +3,6 @@ import random
 import string
 import names
 
-def nombre():
-    return names.get_full_name()
-
 estudiantes = np.empty((6500, 5), dtype=object)
 
 #Columna 1 = Codigo Estudiante
@@ -15,27 +12,27 @@ estudiantes = np.empty((6500, 5), dtype=object)
 #Columna 5 = Año de Ingreso 
 
 estudiantes[:, 0] = np.random.randint(2000000, 3000000, size=6500)
-estudiantes[:, 1] = [nombre() for i in range(6500)]
+estudiantes[:, 1] = [names.get_full_name() for i in range(6500)]
 estudiantes[:, 2] = np.around(np.random.uniform(2.8, 5, size=6500), decimals=1)
 estudiantes[:, 3] = np.random.randint(10, 69, size=6500)
 estudiantes[:, 4] = np.random.randint(1980, 2024, size=6500)
 
-codigo_carrera = input("Ingrese el código de la carrera a listar (los codigos de carreras van de 10 a 68): ")
+codigo_carrera = input("Ingrese el código de la carrera a listar (los codigos de carreras van de 10 a 68): \n")
 estudiante_por_carreras = estudiantes[estudiantes[:,3].astype('str') == codigo_carrera]
 estudiante_por_promedios = estudiante_por_carreras[estudiante_por_carreras[:,2].astype(float) >= 4]
 
-print("Código y nombre de los estudiantes de la carrera ",codigo_carrera," con promedio acumulado igual o mayor a 4:")
+print("Código y nombre de los estudiantes de la carrera ",codigo_carrera," con promedio acumulado igual o mayor a 4:\n")
 
 for estudiante in estudiante_por_promedios:
-    print("Código: ",estudiante[0], ", Nombre: ",estudiante[1], ", Promedio: ",estudiante[2])
+    print("CÓDIGO: ",estudiante[0], ", NOMBRE: ",estudiante[1], ", PROMEDIO: ",estudiante[2],", AÑO DE INGRESO: ",estudiante[4])
     
-print("Total de estudiantes: ",len(estudiante_por_promedios))
+print("\nTotal de estudiantes: ",len(estudiante_por_promedios))
 
 estudiantes_antes_1990 = estudiantes[estudiantes[:,4].astype(int) < 1990]
 estudiantes_condicionales = estudiantes_antes_1990[estudiantes_antes_1990[:,2].astype(float) < 3]
 
-print("Código y nombre de los estudiantes que ingresaron antes de 1990 y están condicionales:")
+print("\nCódigo y nombre de los estudiantes que ingresaron antes de 1990 y están condicionales:\n")
 
 for estudiante in estudiantes_condicionales:
-    print("Código: ",estudiante[0], ", Nombre: ",estudiante[1],", Promedio: ",estudiante[2])
+    print("CÓDIGO: ",estudiante[0], ", NOMBRE: ",estudiante[1], ", PROMEDIO: ",estudiante[2],", AÑO DE INGRESO: ",estudiante[4])
 

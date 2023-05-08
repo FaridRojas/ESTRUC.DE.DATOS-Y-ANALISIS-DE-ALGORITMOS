a72b36da1c6484247fbd3c3bfd14114b42a1fcc7
Farid Rojas <111253744+FaridRojas@users.noreply.github.com>
2023-05-08 08:55:39 -0500

    Agrego Ejercicios

---
+++ Numpy-Exercise/Ejercicio1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+votos = np.random.randint(1, 31, 5000)
+
+votosordenados = np.bincount(votos)
+
+candidatosordenados = np.argsort(votosordenados)[::-1]
+
+for x in candidatosordenados:
+    if x != 0:
+        print("Candidato N°",x," tiene ",votosordenados[x])
+    
+print("Farid Camilo Rojas Vargas - ","2220051")
+print("2023")

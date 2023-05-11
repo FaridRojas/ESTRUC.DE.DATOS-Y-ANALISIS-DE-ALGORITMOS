fdef741d52ac475ad0a9d88b5ffd409c5b3248a4
Farid Rojas <111253744+FaridRojas@users.noreply.github.com>
2023-05-11 07:24:03 -0500

    Create nodos.py

---
+++ Clase/nodos.py
@@ -0,0 +1,34 @@
+class Nodo:
+    #Constructor Nodo
+    def __init__(self, valor):
+        self.valor = valor
+        self.siguiente = None
+        
+    #Metodo Ver el valor    
+    def devolver_valor(self):
+        print(self.valor)
+
+    #Metodo Asignar el siguiente
+    def asignar_siguiente(self,nodo):
+        self.siguiente = nodo
+        
+    #Metodo Cambiar el valor    
+    def cambiar_valor(self,nuevo_valor):
+        self.valor = nuevo_valor
+        
+Nodo1 = Nodo("A")
+Nodo2 = Nodo("C")
+
+#print(Nodo1.valor) 
+
+#Nodo1.devolver_valor()
+
+#Nodo.devolver_valor(Nodo1)
+
+Nodo1.cambiar_valor("B")
+
+Nodo1.devolver_valor()
+
+Nodo1.asignar_siguiente(Nodo2)
+
+print(Nodo1.siguiente)
